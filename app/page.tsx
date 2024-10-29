a5ae53ff0595c1e037d79b66d69e3a30bc7647c8
Zaq Reynolds <zaqreynolds@gmail.com>
2024-10-29 15:39:00 -0700

    feat(layout): Update app title and description, adjust main layout structure
    - Updated the title and description in metadata
    - Restructured the main layout with a flex column
    - Added an input component to the Home page
    - Implemented a useDebounce hook for input debounce

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Hello Hermes
-      </main>
+    <div className="flex flex-col h-full w-full items-center">
+      <p>Hello Hermes</p>
+      <Input className="w-44" />
     </div>
   );
 }
