c9e5dc1474ffcbc70555208503b35ceba17a8820
Zaq Reynolds <zaqreynolds@gmail.com>
2024-10-30 15:24:48 -0700

    feat(context): Add Amadeus context provider for token management
    - Added a new context component to manage Amadeus API token.
    - Removed the old API token fetching logic and related files.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { AmadeusProvider } from "./AmadeusContext";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,9 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main className="flex flex-col items-center sm:items-start h-screen">
-          {children}
-        </main>
+        <AmadeusProvider>
+          <main className="flex flex-col items-center sm:items-start h-screen">
+            {children}
+          </main>
+        </AmadeusProvider>
       </body>
     </html>
   );
