aff3c9b815267cb0a96766b2e332bf58eea96f9b
Zaq Reynolds <zaqreynolds@gmail.com>
2024-10-30 09:20:56 -0700

    feat(ui): Add search functionality with debouncing
    - Added search input fields for origin and destination locations.
    - Implemented debouncing logic for the search input to reduce API calls.
    - Created a new module for handling Amadeus API token data.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,50 @@
+"use client";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useDebounce } from "@/hooks/useDebounce";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [search, setSearch] = useState("");
+  const debouncedValue = useDebounce(search, 500);
+
+  useEffect(() => {
+    if (debouncedValue) {
+      const fetchLocations = async () => {
+        // try {
+        //   const response = await fetch(`https://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/US/en-US?query=${debouncedValue}&apiKey=${process.env.NEXT_PUBLIC_SKYSCANNER_API_KEY}`);
+        //   const data = await response.json();
+        //   console.log(data);
+        // } catch (error) {
+        //   console.error("Error fetching data:", error);
+        // }
+      };
+
+      fetchLocations();
+    }
+  }, [debouncedValue]);
+
   return (
     <div className="flex flex-col h-full w-full items-center">
       <p>Hello Hermes</p>
-      <Input className="w-44" />
+      <Label htmlFor="origin_search">Origin:</Label>
+      <Input
+        id="origin_search"
+        className="w-44"
+        type="text"
+        value={search}
+        placeholder="Search for a location..."
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <Label htmlFor="destination_search">Destination:</Label>
+      <Input
+        id="destination_search"
+        className="w-44"
+        type="text"
+        value={search}
+        placeholder="Search for a location..."
+        onChange={(e) => setSearch(e.target.value)}
+      />
     </div>
   );
 }
