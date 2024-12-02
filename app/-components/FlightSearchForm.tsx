a7f40663fbaec8fb39a107e1ed0072f1745d22f9
Zaq Reynolds <zaqreynolds@gmail.com>
2024-12-01 16:42:58 -0800

    feat(FlightSearch): add flight class selector component
    - Introduced a new FlightClassSelector component for selecting flight classes.
    - Removed inline flight class selection from the FlightSearchForm.
    - Integrated the new selector into the FlightSearchForm, maintaining form control and mobile responsiveness.

--- app/-components/FlightSearchForm.tsx
+++ app/-components/FlightSearchForm.tsx
@@ -45,6 +45,7 @@ import { TravelerSelector } from "./travelerSelector/TravelerSelector";
 import { useFetchLocation } from "./useFetchLocation";
 import { LocationInput } from "./LocationInput";
 import { RoundtripOneWaySelector } from "./RoundtripOneWaySelector";
+import { FlightClassSelector } from "./FlightClassSelector";
 
 export const FlightSearchForm = () => {
   const [isRotated, setIsRotated] = useState<boolean>(false);
@@ -96,44 +97,7 @@ export const FlightSearchForm = () => {
 
             <TravelerSelector control={form.control} isMobile={isMobile} />
 
-            {/* Flight Class Select */}
-            <FormField
-              control={form.control}
-              name="travelClass"
-              render={({ field }) => (
-                <FormItem>
-                  <Select onValueChange={(value) => field.onChange(value)}>
-                    <SelectTrigger
-                      className={cn(
-                        "text-xs hover:bg-accent",
-                        isMobile ? "w-[125px]" : "w-36"
-                      )}
-                    >
-                      <SelectValue placeholder="Flight class" />
-                    </SelectTrigger>
-                    <SelectContent className="w-36">
-                      <SelectGroup>
-                        <SelectItem value="economy" className="text-xs  pr-1 ">
-                          Economy
-                        </SelectItem>
-                        <SelectItem
-                          value="premiumEconomy"
-                          className="text-xs pr-1 "
-                        >
-                          Premium Economy
-                        </SelectItem>
-                        <SelectItem value="business" className="text-xs  pr-1 ">
-                          Business
-                        </SelectItem>
-                        <SelectItem value="first" className="text-xs  pr-1 ">
-                          First
-                        </SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                </FormItem>
-              )}
-            />
+            <FlightClassSelector control={form.control} isMobile={isMobile} />
           </div>
           <div className="relative flex flex-col justify-start pb-2 sm:flex-row gap-1 sm:gap-2">
             {/* Origin Input */}
