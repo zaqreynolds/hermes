b0499ed419743541acc455b238ea3a22af05da83
Zaq Reynolds <zaqreynolds@gmail.com>
2025-01-17 16:40:52 -0800

    feat(flightSearch): add mobile filter toolbar and toggle
    - Introduced a new MobileFilterToolbar component for filtering flight results on mobile.
    - Added state management to toggle the visibility of the filter toolbar.
    - Updated UI to include a filter button that appears only on mobile devices.
    - Enhanced layout for better alignment of flight selection header and filter button.

--- app/_views/flightSearchResults/FlightSearchResults.tsx
+++ app/_views/flightSearchResults/FlightSearchResults.tsx
@@ -4,11 +4,14 @@ import {
   defaultSearchState,
 } from "@/context/FlightSearchContext";
 import { FlightOffer } from "amadeus-ts";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import FlightResultCard from "./FlightResultCard";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
+import { FilterIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { MobileFilterToolbar } from "./toolbar/MobileFilterToolbar";
 
 export const FlightSearchResults = () => {
   const {
@@ -17,6 +20,7 @@ export const FlightSearchResults = () => {
     isFlightSearchLoading,
     selectedAirlines,
   } = useContext(FlightSearchContext);
+  const [isOpen, setIsOpen] = useState(false);
 
   const flightOffers = searchState.flightOffers as FlightOffer[];
   const selectedFlight = searchState.selectedFlight;
@@ -41,7 +45,21 @@ export const FlightSearchResults = () => {
         <div
           className={cn("flex flex-col overflow-hidden", !isMobile && "mr-4")}
         >
-          <h2 className="text-lg font-semibold mb-4">Select your flight:</h2>
+          <div className="flex justify-items-center mb-4">
+            <h2 className="text-lg font-semibold  mr-auto">
+              Select your flight:
+            </h2>
+
+            {isMobile && (
+              <>
+                <Button variant="ghost" onClick={() => setIsOpen(!isOpen)}>
+                  <FilterIcon className="w-6 h-6" />
+                </Button>
+              </>
+            )}
+          </div>
+          {isOpen && <MobileFilterToolbar />}
+
           <div className="flex flex-col gap-1 overflow-y-auto overflow-x-hidden">
             {filteredFlightOffers.map((flight) => (
               <FlightResultCard
